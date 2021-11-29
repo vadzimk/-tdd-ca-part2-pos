4142bb0e3c7a97113da5da96034738b84698a57d
vadzim <vadzimk@hotmail.com>
2021-11-28 22:00:48 -0800

    can sale item for 8.0

--- test/src/link/vadzimk/pos/test/SellOneItemTest.java
+++ test/src/link/vadzimk/pos/test/SellOneItemTest.java
@@ -15,5 +15,23 @@ import static org.junit.Assert.assertEquals;
  * */
 
 public class SellOneItemTest {
+    @Test
+    public void productFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale();
 
+        sale.onBarcode("12345");
+        assertEquals("8.0", display.getText());
+    }
+
+    static class Display{
+        public String getText(){
+            return "8.0";
+        }
+    }
+    static class Sale{
+        public void onBarcode(String barcode){
+
+        }
+    }
 }
