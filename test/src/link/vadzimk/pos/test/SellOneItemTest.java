2d7b45e5a85d7bddc16906b9159f8bbeb94fbc98
vadzim <vadzimk@hotmail.com>
2021-11-28 21:52:57 -0800

    initial setup

---
+++ test/src/link/vadzimk/pos/test/SellOneItemTest.java
@@ -0,0 +1,19 @@
+package link.vadzimk.pos.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * The output of the system is the text of the price
+ * The input is an event of scanning a barcode for the listener onBarcode(code: String): void
+ * Test plan:
+ * product found
+ * product not found
+ * empty barcode
+ * null barcode
+ * */
+
+public class SellOneItemTest {
+
+}
